48c2663c042675fa6077bd2475d2eb27b70de5fc
Nick <idainswan@gmail.com>
2025-09-10 12:35:22 +0300

    команда старта / файл зависимостей, пакет обработчиков

---
+++ handlers/h01_start.py
@@ -0,0 +1,10 @@
+from aiogram import F, Router
+from aiogram.types import Message
+from aiogram.filters import CommandStart
+
+
+router = Router()
+
+@router.message(CommandStart())
+async def start(message: Message):
+    await message.answer("Добро пожаловать, напишите ваш запрос.")
