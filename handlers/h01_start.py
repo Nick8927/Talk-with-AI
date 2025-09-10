ed86633e0b2a0ed1f242168c5f332229be263bf9
Nick <idainswan@gmail.com>
2025-09-10 14:36:11 +0300

    функции защита от флуда, генерации ответа на запрос пользователя / рабочий вариант в минималистичном исполнении

--- handlers/h01_start.py
+++ handlers/h01_start.py
@@ -1,11 +1,33 @@
-from aiogram import F, Router
+from aiogram import Router
 from aiogram.types import Message
 from aiogram.filters import CommandStart
-
+from aiogram.fsm.state import State, StatesGroup
+from aiogram.fsm.context import FSMContext
+from settings_ai.settings import ask
 
 router = Router()
+
+
+class Creator(StatesGroup):
+    wait = State()
+
 
 @router.message(CommandStart())
 async def start(message: Message):
     """обработка команды /start"""
     await message.answer("Я готов дать ответ на любой вопрос\nНапишите его 👇")
+
+
+@router.message(Creator.wait)
+async def stop_flood(message: Message):
+    """защита от флуда"""
+    await message.answer("Ваш запрос обрабатывается,\nпожалуйста подождите 😊")
+
+
+@router.message()
+async def generate(message: Message, state: FSMContext):
+    """обработка запроса и ответ пользователю"""
+    await state.set_state(Creator.wait)
+    response = await ask(message.text)
+    await message.answer(response)
+    await state.clear()
